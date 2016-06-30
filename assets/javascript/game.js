da3b8a2fb42074e47dbb16d27e02dc0dfacf8a78
James <semaj5588@gmail.com>
2016-06-29 23:28:54 -0400

    added stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    //This code will run after your page loads
+
+    $(".yeti").mousedown(function() {
+        alert("GRRR BEWARE THE YETI!");
+    });
+
+    $(".penguin3").mousedown(function() {
+        alert("GRRR BEWARE THE YETI!");
+    });
+
+    $(".penguin8").mousedown(function() {
+        alert("GRRR BEWARE THE YETI!");
+    });
+
+});
